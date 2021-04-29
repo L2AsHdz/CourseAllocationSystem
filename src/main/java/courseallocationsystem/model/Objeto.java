c25b299e872b81fc83fce860616dd62cfcb19592
L2AsHdz <leonidas0197@gmail.com>
2021-04-29 04:13:59 -0600

    Lista simple agregada

---
+++ src/main/java/courseallocationsystem/model/Objeto.java
@@ -0,0 +1,24 @@
+package courseallocationsystem.model;
+
+/**
+ *
+ * @date 29/04/2021
+ * @time 04:12:33
+ * @author asael
+ */
+public class Objeto {
+    
+    private int id;
+
+    public Objeto(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
